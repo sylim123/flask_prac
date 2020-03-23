bf2166fb2b39bdca09c55e1c38c885709c29f3d9
seyoon.Lim <sylim123@naver.com>
2020-03-23 17:34:24 +0900

    completed the db transactions modify and delete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from database import db_session, init_db
 from models.restaurants import Restaurants
+import datetime
 app = Flask(__name__)
 
 
@@ -33,6 +34,49 @@ def create_restaurant():
         return '{}, {}, {}'.format(name, description, site_url)
 
     return render_template('create_restaurant.html')
+
+
+@app.route('/restaurants')
+def restaurant_list():
+    restaurants = Restaurants.query.all()
+
+    return render_template('restaurant.html', restaurants=restaurants)
+
+
+@app.route('/edit-restaurant', methods=['GET', 'POST'])
+def edit_restaurant():
+    id = request.args.get('id')
+
+    restaurant = Restaurants.query.filter(Restaurants.id == id).first()
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        site_url = request.form.get('site_url')
+
+        restaurant.name = name
+        restaurant.description = description
+        restaurant.site_url = site_url
+        restaurant.modified_time = datetime.datetime.now()
+
+        db_session.commit()
+
+        return redirect('/restaurants')
+
+    return render_template('edit_restaurant.html', restaurant=restaurant)
+
+
+@app.route('/delete-restaurant')
+def delete_restaurant():
+    id = request.args.get('id')
+
+    restaurant = Restaurants.query.filter(Restaurants.id == id).first()
+
+    if restaurant:
+        db_session.delete(restaurant)
+        db_session.commit()
+
+    return redirect('/restaurants')
 
 
 if __name__ == '__main__':
